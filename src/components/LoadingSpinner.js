33396ea181332099a8be1fae9e910d4a04a0e660
HALGHASRA <husain.alghasra@gmail.com>
2023-05-18 16:17:01 +0300

    Some adjustments to make the spinner take up the same proportion of space relative to its parent container

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -3,7 +3,7 @@ import CircularProgress from '@mui/material/CircularProgress';
 
 const LoadingSpinner = () => {
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', width:'100%', position: 'absolute', top: 0, left: 0, zIndex: 999 }}>
             <CircularProgress />
         </div>
     );
