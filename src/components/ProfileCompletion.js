1f426d719e8dd0e8806738cb99a936df05ab1414
HALGHASRA <husain.alghasra@gmail.com>
2023-05-20 01:42:20 +0300

    Refined the design of the page

--- src/components/ProfileCompletion.js
+++ src/components/ProfileCompletion.js
@@ -3,18 +3,20 @@ import { TextField, Button } from '@mui/material';
 import { doc, setDoc } from "firebase/firestore";
 import { db, auth } from '../firebase';
 import { useNavigate } from 'react-router-dom'; // to be used in navigating the user to home after profile completion
+import { Box, Snackbar } from '@mui/material'; //Import Box and Snackbar from MUI to improve page UI
 
 function ProfileCompletion({ onProfileComplete }) { //destructure onProfileComplete from props
   const [name, setName] = useState('');
   const [bio, setBio] = useState('');
   const navigate = useNavigate(); // initialise navigate
+  const [error, setError] = useState(false); // Added a new state for handling error display
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     
     // Validating name and bio section
     if (!name || !bio) {
-      console.log('All fields are required');
+      setError(true); // If fields are empty, set error to true
       return;
     }
 
@@ -39,26 +41,54 @@ function ProfileCompletion({ onProfileComplete }) { //destructure onProfileCompl
     }
   };
 
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setError(false);
+  } // this handles the dismissal of the snackbar
+
   return (
-    <form onSubmit={handleSubmit}>
-      <TextField
-        name="name"
-        label="Name"
-        value={name}
-        onChange={event => setName(event.target.value)}
-        required
-      />
-      <TextField
-        name="bio"
-        label="Bio"
-        value={bio}
-        onChange={event => setBio(event.target.value)}
-        required
-      />
-      <Button type="submit">
-        Submit
-      </Button>
-    </form>
+    <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
+      <Box width="500px"> {/* Limit the form width */}
+        <form onSubmit={handleSubmit}>
+          <TextField
+            name="name"
+            label="Name"
+            value={name}
+            onChange={event => setName(event.target.value)}
+            required
+            fullWidth
+            sx={{ mb: 1}} // add bottom margin
+          />
+          <TextField
+            name="bio"
+            label="Bio"
+            value={bio}
+            onChange={event => setBio(event.target.value)}
+            required
+            fullWidth
+            multiline
+            //sx={{ mb: 1}} // add bottom margin
+          />
+          <Button type="submit" fullWidth variant="contained" color="primary" sx={{ mt: 2 }}>
+            Submit
+          </Button>
+        </form>
+        {/* Snackbar for error messages */}
+        <Snackbar
+          open={error}
+          autoHideDuration={6000}
+          onClose={handleClose}
+          message="All fields are required"
+          action={
+            <Button color="secondary" size="small" onClick={handleClose}>
+              Close
+            </Button>
+          }
+        />
+      </Box>
+    </Box>
   );
 }
 
