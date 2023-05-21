9c5fcaa72fab7b3431007ac24c1c4d474144ca87
HALGHASRA <husain.alghasra@gmail.com>
2023-05-21 15:07:54 +0300

    added a colour profile for the app

---
+++ src/theme/Colours.js
@@ -0,0 +1,10 @@
+const Colours = {
+    purple: '#8969FE',
+    yellow: '#DEFE69',
+    white: '#FFFFFF',
+    cyan: '#69FED3',
+    red: '#FE6994',
+    black: '#000000'
+};
+
+export default Colours;
