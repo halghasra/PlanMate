6590c6af24bb71388f948a8ea4fbbaca4a1a19ce
HALGHASRA <husain.alghasra@gmail.com>
2023-05-21 22:12:03 +0300

    added skeleton components for Home and NewEntry

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Home</h1>
+                {/* Content will be added soon */}
+            </div>
+        );
+    }
+}
+
+export default Home;
---
+++ src/components/NewEntry.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class NewEntry extends Component {
+    render() {
+        return (
+            <div>
+                <h1>New Entry</h1>
+                {/* Content will be added soon */}
+            </div>
+        );
+    }
+}
+
+export default NewEntry;
