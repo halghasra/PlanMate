c49e59e414351d84fc84eb254e334eda878058e7
HALGHASRA <husain.alghasra@gmail.com>
2023-07-17 23:45:57 +0300

    Attempting to fix timed events within the calendar

--- src/components/EventPopup.js
+++ src/components/EventPopup.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField, Box } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField, Checkbox, FormControlLabel, Box } from "@mui/material";
 
 const EventPopup = ({ isOpen, onClose, onSubmit }) => {
     const [eventData, setEventData] = useState({
@@ -10,8 +10,18 @@ const EventPopup = ({ isOpen, onClose, onSubmit }) => {
         startTime: "",
         endTime: "",
         notes: "",
+        isFullDay: false, // added isFullDay flag to the state to give the option to the user to select if the event is a full-day event or not
     });
 
+    useEffect(() => {
+        if (isOpen) {
+            setEventData((prevData) => ({
+                ...prevData,
+                isFullDay: false, // Reset the isFullDay flag when the popup is opened
+            }));
+        }
+    }, [isOpen]);
+    
     const handleChange = (e) => {
         const { name, value } = e.target;
         setEventData((prevData) => ({
@@ -20,8 +30,23 @@ const EventPopup = ({ isOpen, onClose, onSubmit }) => {
         }));
     };
 
+    // added function to handle the checkbox change event
+    const handleCheckboxChange = (e) => {
+        const { name, checked } = e.target;
+        setEventData((prevData) => ({
+            ...prevData,
+            [name]: checked,
+        }));
+    };
+
     const handleSubmit = () => {
-        onSubmit(eventData);
+        // Remove the isFullDay flag from the eventData object
+        const { isFullDay, ...eventDataWithoutIsFullDay } = eventData;
+        const event = {
+            ...eventDataWithoutIsFullDay,
+            isFullDay,
+        };
+        onSubmit(event);
         setEventData({
             title: "",
             eventType: "",
@@ -30,6 +55,7 @@ const EventPopup = ({ isOpen, onClose, onSubmit }) => {
             startTime: "",
             endTime: "",
             notes: "",
+            isFullDay: false,
         });
     };
 
@@ -89,6 +115,18 @@ const EventPopup = ({ isOpen, onClose, onSubmit }) => {
                     />
                 </Box>
                 <Box sx={{ marginBottom: 2 }}>
+                    <FormControlLabel
+                        control={
+                            <Checkbox
+                                name="isFullDay"
+                                checked={eventData.isFullDay}
+                                onChange={handleCheckboxChange}
+                            />
+                        }
+                        label="Full-day event"
+                    />
+                </Box>
+                <Box sx={{ marginBottom: 2 }}>
                     <TextField
                         name="startTime"
                         label="Start Time"
@@ -99,6 +137,7 @@ const EventPopup = ({ isOpen, onClose, onSubmit }) => {
                             shrink: true,
                         }}
                         fullWidth
+                        disabled={eventData.isFullDay} // Disable start time field if it's a full-day event
                     />
                 </Box>
                 <Box sx={{ marginBottom: 2 }}>
@@ -112,6 +151,7 @@ const EventPopup = ({ isOpen, onClose, onSubmit }) => {
                             shrink: true,
                         }}
                         fullWidth
+                        disabled={eventData.isFullDay} // Disable end time field if it's a full-day event
                     />
                 </Box>
                 <Box sx={{ marginBottom: 2 }}>
--- src/components/FullCalendar.js
+++ src/components/FullCalendar.js
@@ -1,109 +1,157 @@
 import React, { useState, useEffect } from "react";
-import { ThemeProvider, Box, Button } from "@mui/material";
+import { ThemeProvider, Box } from "@mui/material";
 import FullCalendar from "@fullcalendar/react";
+import timeGridPlugin from "@fullcalendar/timegrid";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import theme from "../theme/theme";
-import { collection, getDocs, addDoc, serverTimestamp } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  serverTimestamp,
+} from "firebase/firestore";
 import { db } from "../firebase";
 import EventPopup from "./EventPopup";
+import { nanoid } from "nanoid";
 
-export default function Calendar({ user}) {
-    // State variable to store events
-    const [events, setEvents] = useState([]);
-    // State variable to control the popup
-    const [isPopupOpen, setPopupOpen] = useState(false); 
+export default function Calendar({ user }) {
+  // State variable to store events
+  const [events, setEvents] = useState([]);
+  // State variable to control the popup
+  const [isPopupOpen, setPopupOpen] = useState(false);
 
-    useEffect(() => {
-      // Fetch events from Firestore
-      const fetchEvents = async () => {
-        const eventsCollection = collection(db, "events");
-        const eventsSnapshot = await getDocs(eventsCollection);
-        // Map the events data and update the state
-        const eventsData = eventsSnapshot.docs.map((doc) => ({
-          id: doc.id,
-          title: doc.data().title,
-          start: doc.data().startDate, // specify the start property
-          end: doc.data().endDate, // specify the end property
-          ...doc.data(),
-        }));
-        setEvents(eventsData);
-      };
-  
-      fetchEvents();
-    }, []);
-
-    // Function to handle date click events on the calendar
-    const handleDateClick = () => {
-      setPopupOpen(true);
+  useEffect(() => {
+    // Fetch events from Firestore
+    const fetchEvents = async () => {
+      const eventsCollection = collection(db, "events");
+      const eventsSnapshot = await getDocs(eventsCollection);
+      // Map the events data and update the state
+      const eventsData = eventsSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        title: doc.data().title,
+        start: doc.data().start, // Change v3: use the start date directly from Firestore
+        end: doc.data().end, // Same thing for the end date
+        ...doc.data(),
+      }));
+      setEvents(eventsData);
     };
 
-    // Function to handle event create on the calendar
-    const handleEventCreate = async (eventData) => {
-      // Destructure the eventData
-      const { title, eventType, startDate, endDate, startTime, endTime, notes } = eventData;
-      // Create a new event
-      const event = {
-        title,
-        eventType,
-        startDate,
-        endDate,
-        startTime,
-        endTime,
-        notes,
-        userId: user.uid,
-        createdAt: serverTimestamp(),
+    fetchEvents();
+  }, []);
+
+  // Function to handle date click events on the calendar
+  const handleDateClick = () => {
+    setPopupOpen(true);
+  };
+
+  // Function to handle event create on the calendar
+  const handleEventCreate = async (eventData) => {
+    // Destructure the eventData
+    const {
+      title,
+      eventType,
+      start,
+      end,
+      startTime,
+      endTime,
+      notes,
+      isFullDay,
+    } = eventData;
+
+    let eventStart = start;
+    let eventEnd = end;
+    let groupId = nanoid(); // Generate a unique groupId for the event using nanoid
+
+    if (isFullDay) {
+      // If it's a full-day event, adjust the end date by adding one day
+      const nextDay = new Date(end);
+      nextDay.setDate(nextDay.getDate() + 1);
+      eventEnd = nextDay.toISOString().split("T")[0];
+    } else if (!isFullDay && startTime && endTime) {
+      // If both start time and end time are provided, it's a timed event
+      eventStart = `${start}T${startTime}:00`;
+      eventEnd = `${end}T${endTime}:00`;
+    }
+
+    // Create a new event
+    const event = {
+      title,
+      start: eventStart,
+      end: eventEnd,
+      allDay: isFullDay,
+      eventType,
+      startTime,
+      endTime,
+      notes,
+      groupId,
+      userId: user.uid,
+      createdAt: serverTimestamp(),
     };
-    
+
     // Add the event to Firestore
     const docRef = await addDoc(collection(db, "events"), event);
     setEvents((prevEvents) => [
       ...prevEvents,
-      { id: docRef.id, title, start: startDate, end: endDate, ...event },
+      {
+        id: docRef.id,
+        title,
+        start: eventStart,
+        end: eventEnd,
+        groupId,
+        ...event,
+      },
     ]);
     // Close the popup
     setPopupOpen(false);
-    };
+  };
 
-    // Function to render event content
-    const renderEventContent = (eventInfo) => {
-        return (
-            <>
-                <b>{eventInfo.timeText}</b>
-                <i>{eventInfo.event.title}</i>
-            </>
-        );
-    };
-
+  // Function to render event content
+  const renderEventContent = (eventInfo) => {
     return (
-        <ThemeProvider theme={theme}>
-          <Box
-            sx={{
-              height: "100hv",
-              width: "100%",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
+      <>
+        <b>{eventInfo.timeText}</b>
+        <i>{eventInfo.event.title}</i>
+      </>
+    );
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+          height: "100hv",
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box sx={{ width: "90%" }}>
+          <FullCalendar
+            plugins={[timeGridPlugin, dayGridPlugin, interactionPlugin]}
+            initialView="dayGridMonth"
+            headerToolbar={{
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay",
             }}
-          >
-            <Box sx={{ width: "90%" }}>
-              <FullCalendar
-                plugins={[dayGridPlugin, interactionPlugin]}
-                initialView="dayGridMonth"
-                weekends={false}
-                dateClick={handleDateClick}
-                events={events}
-                eventContent={renderEventContent}
-              />
-            </Box>
-          </Box>
-          <EventPopup
-            isOpen={isPopupOpen}
-            onClose={() => setPopupOpen(false)}
-            onSubmit={handleEventCreate}
+            slotDuration="00:15" // Specify the duration of each slot
+            slotMinTime="00:00:00" // Specify the minimum time to display in slots
+            slotMaxTime="24:00:00" // Specify the maximum time to display in slots
+            allDaySlot={false} // Disable all-day slot
+            dateClick={handleDateClick}
+            events={events}
+            eventContent={renderEventContent}
+            editable={true}
           />
-        </ThemeProvider>
-      );
-    }
-
-
+        </Box>
+      </Box>
+      <EventPopup
+        isOpen={isPopupOpen}
+        onClose={() => setPopupOpen(false)}
+        onSubmit={handleEventCreate}
+      />
+    </ThemeProvider>
+  );
+}
