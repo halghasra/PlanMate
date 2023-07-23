6611d7efdb82a7f6d6f75cb8843159c10f9eb339
HALGHASRA <husain.alghasra@gmail.com>
2023-07-23 19:30:44 +0300

    Fixed the issue with event display on the calendar

--- src/components/EventPopup.js
+++ src/components/EventPopup.js
@@ -1,181 +1,145 @@
 import React, { useEffect, useState } from "react";
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField, Checkbox, FormControlLabel, Box } from "@mui/material";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Box,
+} from "@mui/material";
 
 const EventPopup = ({ isOpen, onClose, onSubmit }) => {
-    const [eventData, setEventData] = useState({
-        title: "",
-        eventType: "",
-        startDate: "",
-        endDate: "",
-        startTime: "",
-        endTime: "",
-        notes: "",
-        isFullDay: false, // added isFullDay flag to the state to give the option to the user to select if the event is a full-day event or not
+  const [eventData, setEventData] = useState({
+    title: "",
+    start: "",
+    end: "",
+    notes: "",
+    isFullDay: false, // added isFullDay flag to the state to give the option to the user to select if the event is a full-day event or not
+  });
+
+  useEffect(() => {
+    if (isOpen) {
+      setEventData((prevData) => ({
+        ...prevData,
+        isFullDay: false, // Reset the isFullDay flag when the popup is opened
+      }));
+    }
+  }, [isOpen]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEventData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  // added function to handle the checkbox change event
+  const handleCheckboxChange = (e) => {
+    const { name, checked } = e.target;
+    setEventData((prevData) => ({
+      ...prevData,
+      [name]: checked,
+    }));
+  };
+
+  const handleSubmit = () => {
+    // Remove the isFullDay flag from the eventData object
+    const { isFullDay, ...eventDataWithoutIsFullDay } = eventData;
+    const event = {
+      ...eventDataWithoutIsFullDay,
+      isFullDay,
+    };
+    onSubmit(event);
+    setEventData({
+      title: "",
+      start: "",
+      end: "",
+      notes: "",
+      isFullDay: false,
     });
+  };
 
-    useEffect(() => {
-        if (isOpen) {
-            setEventData((prevData) => ({
-                ...prevData,
-                isFullDay: false, // Reset the isFullDay flag when the popup is opened
-            }));
-        }
-    }, [isOpen]);
-    
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setEventData((prevData) => ({
-            ...prevData,
-            [name]: value,
-        }));
-    };
-
-    // added function to handle the checkbox change event
-    const handleCheckboxChange = (e) => {
-        const { name, checked } = e.target;
-        setEventData((prevData) => ({
-            ...prevData,
-            [name]: checked,
-        }));
-    };
-
-    const handleSubmit = () => {
-        // Remove the isFullDay flag from the eventData object
-        const { isFullDay, ...eventDataWithoutIsFullDay } = eventData;
-        const event = {
-            ...eventDataWithoutIsFullDay,
-            isFullDay,
-        };
-        onSubmit(event);
-        setEventData({
-            title: "",
-            eventType: "",
-            startDate: "",
-            endDate: "",
-            startTime: "",
-            endTime: "",
-            notes: "",
-            isFullDay: false,
-        });
-    };
-
-    return (
-        <Dialog open={isOpen} onClose={onClose}>
-            <Box sx={{ minWidth: 600 }}>
-            <DialogTitle>Create Event</DialogTitle>
-            <DialogContent>
-                <Box sx={{ marginBottom: 2, marginTop: 2 }}>
-                    <TextField
-                        name="title"
-                        label="Title"
-                        value={eventData.title}
-                        onChange={handleChange}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        fullWidth
-                    />
-                </Box>
-                <Box sx={{ marginBottom: 2 }}>
-                    <TextField
-                        name="eventType"
-                        label="Event Type"
-                        value={eventData.eventType}
-                        onChange={handleChange}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        fullWidth
-                    />
-                </Box>
-                <Box sx={{ marginBottom: 2 }}>
-                    <TextField
-                        name="startDate"
-                        label="Start Date"
-                        type="date"
-                        value={eventData.startDate}
-                        onChange={handleChange}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        fullWidth
-                    />
-                </Box>
-                <Box sx={{ marginBottom: 2 }}>
-                    <TextField
-                        name="endDate"
-                        label="End Date"
-                        type="date"
-                        value={eventData.endDate}
-                        onChange={handleChange}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        fullWidth
-                    />
-                </Box>
-                <Box sx={{ marginBottom: 2 }}>
-                    <FormControlLabel
-                        control={
-                            <Checkbox
-                                name="isFullDay"
-                                checked={eventData.isFullDay}
-                                onChange={handleCheckboxChange}
-                            />
-                        }
-                        label="Full-day event"
-                    />
-                </Box>
-                <Box sx={{ marginBottom: 2 }}>
-                    <TextField
-                        name="startTime"
-                        label="Start Time"
-                        type="time"
-                        value={eventData.startTime}
-                        onChange={handleChange}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        fullWidth
-                        disabled={eventData.isFullDay} // Disable start time field if it's a full-day event
-                    />
-                </Box>
-                <Box sx={{ marginBottom: 2 }}>
-                    <TextField
-                        name="endTime"
-                        label="End Time"
-                        type="time"
-                        value={eventData.endTime}
-                        onChange={handleChange}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        fullWidth
-                        disabled={eventData.isFullDay} // Disable end time field if it's a full-day event
-                    />
-                </Box>
-                <Box sx={{ marginBottom: 2 }}>
-                    <TextField
-                        name="notes"
-                        label="Notes"
-                        multiline
-                        rows={4}
-                        value={eventData.notes}
-                        onChange={handleChange}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        fullWidth
-                    />
-                </Box>
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={onClose}>Cancel</Button>
-                <Button onClick={handleSubmit}>Create</Button>
-            </DialogActions>
-            </Box>
-        </Dialog>
-    );
+  return (
+    <Dialog open={isOpen} onClose={onClose}>
+      <Box sx={{ minWidth: 600 }}>
+        <DialogTitle>Add Event</DialogTitle>
+        <DialogContent>
+          <Box sx={{ marginBottom: 2, marginTop: 2 }}>
+            <TextField
+              name="title"
+              label="Title"
+              value={eventData.title}
+              onChange={handleChange}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              fullWidth
+            />
+          </Box>
+          <Box sx={{ marginBottom: 2 }}>
+            <TextField
+              name="start"
+              label="Start"
+              type="datetime-local"
+              value={eventData.start}
+              onChange={handleChange}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              fullWidth
+            />
+          </Box>
+          <Box sx={{ marginBottom: 2 }}>
+            <TextField
+              name="end"
+              label="End"
+              type="datetime-local"
+              value={eventData.end}
+              onChange={handleChange}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              fullWidth
+            />
+          </Box>
+          <Box sx={{ marginBottom: 2 }}>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  name="isFullDay"
+                  checked={eventData.isFullDay}
+                  onChange={handleCheckboxChange}
+                />
+              }
+              label="Full-day event"
+            />
+          </Box>
+          <Box sx={{ marginBottom: 2 }}>
+            <TextField
+              name="notes"
+              label="Notes"
+              multiline
+              rows={4}
+              value={eventData.notes}
+              onChange={handleChange}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              fullWidth
+            />
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose}>Cancel</Button>
+          <Button onClick={handleSubmit}>Create</Button>
+        </DialogActions>
+      </Box>
+    </Dialog>
+  );
 };
 
 export default EventPopup;
--- src/components/FullCalendar.js
+++ src/components/FullCalendar.js
@@ -29,9 +29,6 @@ export default function Calendar({ user }) {
       // Map the events data and update the state
       const eventsData = eventsSnapshot.docs.map((doc) => ({
         id: doc.id,
-        title: doc.data().title,
-        start: doc.data().start, // Change v3: use the start date directly from Firestore
-        end: doc.data().end, // Same thing for the end date
         ...doc.data(),
       }));
       setEvents(eventsData);
@@ -47,47 +44,14 @@ export default function Calendar({ user }) {
 
   // Function to handle event create on the calendar
   const handleEventCreate = async (eventData) => {
-    // Destructure the eventData
-    const {
-      title,
-      eventType,
-      startDate,
-      endDate,
-      startTime,
-      endTime,
-      notes,
-      isFullDay,
-    } = eventData;
-
-    let eventStart = startDate;
-    let eventEnd = endDate;
-    let groupId = nanoid(); // Generate a unique groupId for the event using nanoid
-
-    if (isFullDay) {
-      // If it's a full-day event, adjust the end date by adding one day
-      const nextDay = new Date(endDate);
-      nextDay.setDate(nextDay.getDate() + 1);
-      eventEnd = nextDay.toISOString();
-    } else if (!isFullDay && startTime && endTime) {
-      // If both start time and end time are provided, it's a timed event
-      eventStart = `${startDate}T${startTime}:00`;
-      eventEnd = `${endDate}T${endTime}:00`;
-    }
-
-    // Create a new event
+    // Destructure the eventData object
+    const groupId = nanoid();
     const event = {
-      title,
-      start: eventStart,
-      end: eventEnd,
-      allDay: isFullDay,
-      eventType,
-      startTime,
-      endTime,
-      notes,
       groupId,
       userId: user.uid,
       createdAt: serverTimestamp(),
-    };
+      ...eventData,
+    }
 
     // Add the event to Firestore
     const docRef = await addDoc(collection(db, "events"), event);
@@ -95,9 +59,6 @@ export default function Calendar({ user }) {
       ...prevEvents,
       {
         id: docRef.id,
-        title,
-        start: eventStart,
-        end: eventEnd,
         groupId,
         ...event,
       },
